820e3e4b9a11b13f4bd4a1d9fce50582e768e7a9
pratikshapaudyal <pratiksha_paudyal@hotmail.co.uk>
2021-11-21 14:22:53 +0000

    Add ner nltk exercises and docs

---
+++ NamedEntityRecognition/nltk_pie_chart.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+import matplotlib.pyplot as plt
+
+# Create the defaultdict: ner_categories
+ner_categories = defaultdict(int)
+
+chunked_sentences = []
+
+# Create the nested for loop
+for sent in chunked_sentences:
+    for chunk in sent:
+        if hasattr(chunk, "label"):
+            ner_categories[chunk.label()] += 1
+
+# Create a list from the dictionary keys for the chart labels: labels
+labels = list(ner_categories.keys())
+
+# Create a list of the values: values
+values = [ner_categories.get(v) for v in labels]
+
+# Create the pie chart
+plt.pie(values, labels=labels, autopct="%1.1f%%", startangle=140)
+
+# Display the chart
+plt.show()
